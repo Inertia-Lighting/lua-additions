0dc4561697ebb7559f0de3f69eca7fd25e91f367
MidSpike <developer@midspike.com>
2023-06-08 14:00:27 -0400

    updates

--- MainModule/src/Color.lua
+++ MainModule/src/Color.lua
@@ -12,19 +12,23 @@ local Color = {}
 --- @param blue number (0-255)
 --- @returns hue number (0-360), saturation number (0-1), value number (0-1)
 function Color.rgbToHsv(red, green, blue)
-    red, green, blue = red / 255, green / 255, blue / 255
-    local maxVal = math.max(red, green, blue)
-    local minVal = math.min(red, green, blue)
+    local r = red / 255
+    local g = green / 255
+    local b = blue / 255
+
+    local maxVal = math.max(r, g, b)
+    local minVal = math.min(r, g, b)
+
     local hue, saturation, value
 
     if maxVal == minVal then
         hue = 0
-    elseif maxVal == red then
-        hue = (60 * ((green - blue) / (maxVal - minVal))) % 360
-    elseif maxVal == green then
-        hue = (60 * ((blue - red) / (maxVal - minVal)) + 120) % 360
-    elseif maxVal == blue then
-        hue = (60 * ((red - green) / (maxVal - minVal)) + 240) % 360
+    elseif maxVal == r then
+        hue = (60 * ((g - b) / (maxVal - minVal))) % 360
+    elseif maxVal == g then
+        hue = (60 * ((b - r) / (maxVal - minVal)) + 120) % 360
+    elseif maxVal == b then
+        hue = (60 * ((r - g) / (maxVal - minVal)) + 240) % 360
     end
 
     if maxVal == 0 then
@@ -38,17 +42,17 @@ function Color.rgbToHsv(red, green, blue)
     return hue, saturation, value
 end
 
---- Converts RGB color values to HTML color code
+--- Converts RGB color values to hex string
 --- @param red number (0-255)
 --- @param green number (0-255)
 --- @param blue number (0-255)
---- @returns htmlColorCode string
-function Color.rgbToHtml(red, green, blue)
-    red = string.format("%02X", red)
-    green = string.format("%02X", green)
-    blue = string.format("%02X", blue)
+--- @returns hexColorString string
+function Color.rgbToHex(red, green, blue)
+    local r = string.format("%02X", red)
+    local g = string.format("%02X", green)
+    local b = string.format("%02X", blue)
 
-    return "#" .. red .. green .. blue
+    return "#" .. r .. g .. b
 end
 
 --- Converts HSV color values to RGB color values
@@ -87,23 +91,24 @@ function Color.hsvToRgb(hue, saturation, value)
     return math.floor(red + 0.5), math.floor(green + 0.5), math.floor(blue + 0.5)
 end
 
---- Converts HSV color values to HTML color code
+--- Converts HSV color values to hex string
 --- @param hue number (0-360)
 --- @param saturation number (0-1)
 --- @param value number (0-1)
---- @returns htmlColorCode string
-function Color.hsvToHtml(hue, saturation, value)
+--- @returns hexColorString string
+function Color.hsvToHex(hue, saturation, value)
     local red, green, blue = Color.hsvToRgb(hue, saturation, value)
-    return Color.rgbToHtml(red, green, blue)
+
+    return Color.rgbToHex(red, green, blue)
 end
 
---- Converts HTML color code to RGB color values
---- @param htmlColorCode string
+--- Converts hex string to RGB color values
+--- @param hexColorString string
 --- @returns red number (0-255), green number (0-255), blue number (0-255)
-function Color.htmlToRgb(htmlColorCode)
-    local redHex = htmlColorCode:sub(2, 3)
-    local greenHex = htmlColorCode:sub(4, 5)
-    local blueHex = htmlColorCode:sub(6, 7)
+function Color.hexToRgb(hexColorString)
+    local redHex = hexColorString:sub(2, 3)
+    local greenHex = hexColorString:sub(4, 5)
+    local blueHex = hexColorString:sub(6, 7)
 
     local red = tonumber(redHex, 16)
     local green = tonumber(greenHex, 16)
@@ -112,12 +117,15 @@ function Color.htmlToRgb(htmlColorCode)
     return red, green, blue
 end
 
---- Converts HTML color code to HSV color values
---- @param htmlColorCode string
+--- Converts hex string to HSV color values
+--- @param hexColorString string
 --- @returns hue number (0-360), saturation number (0-1), value number (0-1)
-function Color.htmlToHsv(htmlColorCode)
-    local red, green, blue = Color.htmlToRgb(htmlColorCode)
+function Color.hexToHsv(hexColorString)
+    local red, green, blue = Color.hexToRgb(hexColorString)
+
     return Color.rgbToHsv(red, green, blue)
 end
+
+--------------------------------------------------------------------------------------------------------------------------------
 
 return Color
