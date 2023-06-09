85001f4c84af61986d6891d0dc0260a268fedd3c
MidSpike <developer@midspike.com>
2023-06-09 15:29:31 -0400

    cleanup + tests

--- MainModule/src/Integer.lua
+++ MainModule/src/Integer.lua
@@ -6,20 +6,20 @@ local Integer = {}
 
 --------------------------------------------------------------------------------------------------------------------------------
 
---- Divides a number and returns the floor and ceil of the result as a tuple
---- @param input number
+--- Divides and returns the floor and ceil of a quotient as a tuple.
+--- @param dividend number
 --- @param divisor number
---- @returns floor number, ceil number
+--- @returns quotientFloor number, quotientCeil number
 --- ```lua
---- local smaller, larger = Integer.divide(7, 2)
---- print(smaller, larger) -- 3, 4
+--- local qMin, qMax = Integer.divide(10, 3)
+--- print(qMin, qMax) -- 3, 4
 --- ```
-function Integer.divide(input, divisor)
+function Integer.divide(dividend, divisor)
     if divisor == 0 then
         error('LA.Integer.divide: divisor cannot be 0')
     end
 
-    local result = input / divisor
+    local result = dividend / divisor
 
     return math.floor(result), math.ceil(result)
 end
